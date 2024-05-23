adadf750d171aa6805d84fc3e0081d897cc42eac
Tora-Dark <110999407+Tora-Dark@users.noreply.github.com>
2024-05-23 17:53:50 -0400

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
         'paths' => ['api/*', 'sanctum/csrf-cookie'],
         'allowed_methods' => ['*'],
-        'allowed_origins' => ['http://localhost:5173'], // URL de tu frontend
+        'allowed_origins' => ['http://localhost:5173/Horario/'], // URL de tu frontend
         'allowed_origins_patterns' => [],
         'allowed_headers' => ['*'],
         'exposed_headers' => [],
