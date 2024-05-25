53aa5232908a8000d5e91aea5ac3c2db934658fc
Tora-Dark <110999407+Tora-Dark@users.noreply.github.com>
2024-05-24 21:43:20 -0400

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
         'allowed_methods' => ['*'],
         'allowed_origins' => ['http://localhost:5173'], // URL de tu frontend
         'allowed_origins_patterns' => [],
-        'allowed_headers' => ['http://localhost:5173'],
+        'allowed_headers' => ['*'],
         'exposed_headers' => [],
         'max_age' => 0,
         'supports_credentials' => false,
